75dc4af150f7bf5e67253e85abea6200f5ded179
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-07-17 16:48:20 +1000

    Begin tests

--- src/test/membership.js
+++ src/test/membership.js
@@ -12,8 +12,8 @@ contract("MembershipVerificationToken", function(accounts) {
   });
 
   describe.only("MembershipVerificationToken tests", () => {
-    it("should add attibute set", async function () {
-      await contractInstance.addAttributeSet("0x68616972", ["0x626c61636b", "0x7768697465"]);
+    it("should add member level attibute set", async function () {
+      await contractInstance.addAttributeSet("0x4c6576656c", ["0x4c696665"]);
 
       // const actual = await tokenInstance._inventory(0);
       // assert.equal(Number(actual[0]), 100, "Stock not correct");
